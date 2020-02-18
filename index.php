a89cc0f88ec2a449ac321536ad906f08ae6bbe0d
abdullah-PC\abdullah <abdullahsyedhussain19>
2020-02-18 22:31:13 +0500

    KT-SRS-Styling-01

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
 <!-- head Start -->
 <head>
 	<title>KT Take Home Assignment</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
 	<?php include("include/header.php"); ?>  
 	
@@ -13,7 +15,33 @@
 <!-- head End-->
 <!-- body Start -->
 <body>
-	
+
+	<div class="container">
+		<div class="row">
+			<div class="col">
+
+				<div class="panel panel-default">
+					<div class="panel-heading">
+						<h3 class="panel-title">Artist Linkedin</h3>
+					</div>
+					<div class="panel-body">
+						<div class="input-group">
+						<input type="text" class="form-control" placeholder="Search Artist">
+						<div class="input-group-append">
+								<button id="btn-search-artist" class="btn btn-secondary" type="button">
+									Search
+								</button>
+							</div>
+						</div>
+					</div>
+				</div>
+			
+					
+			</div>
+
+		</div>
+	</div>
+</div>
 </body>
 <!-- body End-->
 
