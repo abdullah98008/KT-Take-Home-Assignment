78a00d5576cfbeaca26f4798510afb100f528cc2
abdullah-PC\abdullah <abdullahsyedhussain19>
2020-02-18 21:44:25 +0500

    Jquery Added

--- include/footer.php
+++ include/footer.php
@@ -1,1 +1,4 @@
+<!-- jquery 1.12.1 -->
+<script src="js/libs/jquery.js" ></script>
+<!-- bootstrap 4 -->
 <script src="js/libs/bootstrap.min.js" ></script>
--- include/header.php
+++ include/header.php
@@ -1,1 +1,2 @@
+<!-- bootstrap 4 -->
 <link rel="stylesheet" href="css/libs/bootstrap.min.css" >
